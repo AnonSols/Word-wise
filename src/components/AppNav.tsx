373e0961058ba44874f281f8c9db422e9171608b
Anon Sols <soloschmail@gmail.com>
2024-01-31 13:11:32 +0100

    initial commit

---
+++ src/components/AppNav.tsx
@@ -0,0 +1,11 @@
+import style from "./applayout.module.css";
+
+const AppNav = () => {
+  return (
+    <nav className={style.nav}>
+      <p>This is the app nav</p>
+    </nav>
+  );
+};
+
+export default AppNav;
