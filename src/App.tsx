71f805483563a369668abb455389b3ebe06c375f
Anon Sols <soloschmail@gmail.com>
2024-02-01 13:41:52 +0100

    work on static pages and several components

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
+import PageNav
+ from "./components/PageNav
+";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="app" element={<PageNav
+         />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
       </Routes>
