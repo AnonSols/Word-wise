34508a2f7bb488597db10ad6c170b06321a16338
Anon Sols <soloschmail@gmail.com>
2024-02-10 15:18:44 +0100

    error fixed, add map to the map container

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,18 +1,26 @@
 // import { useNavigate, useSearchParams } from "react-router-dom";
+import { useState } from "react";
 import style from "./Map.module.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const Map = () => {
   // const [searchParams, setSearchParams] = useSearchParams();
   // const navigate = useNavigate();
+
+  const [mapPosition, setMapPosition] = useState<[number, number]>([40, 0]);
   return (
     <div className={style.mapContainer}>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={style.map}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={mapPosition}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
